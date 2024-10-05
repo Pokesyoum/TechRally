b93c4c4c2024f11c520e764ac015fc0c6438755c
Pokesyoum <syoumpm@gmail.com>
2024-10-05 13:27:08 +0900

    createアクションの設定

--- app/controllers/rallies_controller.rb
+++ app/controllers/rallies_controller.rb
@@ -7,7 +7,23 @@ class RalliesController < ApplicationController
     @rally = Rally.new
   end
 
+  def create
+    @rally = Rally.new(rally_params)
+    if @rally.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def show
     @rally = Rally.find(params[:id])
+  end
+
+  private
+
+  def rally_params
+    params.require(:rally).permit(:title, :abstract, :background, :idea, :method, :result,
+                                  :discussion, :conclusion, :opinion).merge(user_id: current_user.id)
   end
 end
