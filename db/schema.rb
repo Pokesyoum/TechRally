9c979b68015c83d9210df216436a87f32ebd807f
Pokesyoum <syoumpm@gmail.com>
2024-10-06 15:57:02 +0900

    missionsテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_10_05_234101) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_06_065205) do
   create_table "comments", charset: "utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "rally_id", null: false
@@ -19,6 +19,12 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_05_234101) do
     t.datetime "updated_at", null: false
     t.index ["rally_id"], name: "index_comments_on_rally_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "missions", charset: "utf8", force: :cascade do |t|
+    t.string "description", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "rallies", charset: "utf8", force: :cascade do |t|
