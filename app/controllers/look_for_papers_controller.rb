2ae0c9f09886efa8401ae75a73da753e7069d50f
Pokesyoum <syoumpm@gmail.com>
2024-10-13 09:47:27 +0900

    フォームの作成

--- app/controllers/look_for_papers_controller.rb
+++ app/controllers/look_for_papers_controller.rb
@@ -2,5 +2,18 @@ class LookForPapersController < ApplicationController
   def show
     @user = current_user
     @look_for_papers = LookForPaper.where(user_id: current_user.id)
+    @look_for_paper = LookForPaper.new
+  end
+
+  def create
+    @user = current_user
+    look_for_paper = LookForPaper.create(look_for_paper_params)
+    redirect_to look_for_paper_parh(@user)
+  end
+
+  private
+
+  def look_for_paper_params
+    params.require(:look_for_paper).permit(:look_for_paper, :journal, :search_word).merge(user_id: current_user.id)
   end
 end
