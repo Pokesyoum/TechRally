62c23bbedd81c1bed8fff9ddfac3e980782199f6
Pokesyoum <syoumpm@gmail.com>
2024-10-15 20:53:42 +0900

    whenever gemを用いたミッション配布機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,24 @@ class User < ApplicationRecord
   has_many :missions, through: :user_missions
   has_many :look_for_papers
   has_many :paper_stock
+
+  def self.with_few_missions(limit = 3)
+    joins(:user_missions)
+      .group('users.id')
+      .having('COUNT(user_missions.id) <= ?', limit)
+  end
+
+  def self.assign_missions(mission_id)
+    with_few_missions.each do |user|
+      available_mission_ids = (1..5).to_a - user.mission_ids
+      next if available_mission_ids.empty?
+
+      mission_id = available_mission_ids.sample
+      user.assign_new_mission(mission_id)
+    end
+  end
+
+  def assign_new_mission(mission_id)
+    user_missions.create(mission_id:, completed: false)
+  end
 end
