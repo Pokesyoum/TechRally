04227259fc4bc59329f0f0f90224a56b180e2c35
Pokesyoum <syoumpm@gmail.com>
2024-10-28 21:12:27 +0900

    FactoryBotファイルの作成

---
+++ spec/factories/paper_stock
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :paper_stock do
+    association :user
+    outline   { Faker::Lorem.sentence }
+    paper_url { Faker::Lorem.sentence }
+  end
+end
---
+++ spec/factories/rallies.rb
@@ -0,0 +1,16 @@
+FactoryBot.define do
+  factory :rally do
+    association :user
+    title      { Faker::Lorem.sentence }
+    abstract   { Faker::Lorem.paragraph }
+    background { Faker::Lorem.paragraph }
+    idea       { Faker::Lorem.paragraph }
+    method     { Faker::Lorem.paragraph }
+    result     { Faker::Lorem.paragraph }
+    discussion { Faker::Lorem.paragraph }
+    conclusion { Faker::Lorem.paragraph }
+    opinion    { Faker::Lorem.paragraph }
+    URL        { Faker::Lorem.sentence }
+    draft      { [true, false].sample }
+  end
+end
