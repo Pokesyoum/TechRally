e2d278ffbc918f65e262f1f2c8b0d9a726e1da22
Pokesyoum <syoumpm@gmail.com>
2024-11-08 22:27:29 +0900

    look_for_papersコントローラーの単体テストコード

---
+++ spec/requests/look_for_papers_spec.rb
@@ -0,0 +1,62 @@
+require 'rails_helper'
+
+RSpec.describe LookForPapersController, type: :request do
+  include Devise::Test::IntegrationHelpers
+  before do
+    @user = FactoryBot.create(:user)
+    @rally = FactoryBot.create(:rally, draft: false)
+    @look_for_paper = FactoryBot.create(:look_for_paper, user_id: @user.id)
+    sign_in @user
+
+    basic_auth = ActionController::HttpAuthentication::Basic.encode_credentials(ENV['BASIC_AUTH_USER'],
+                                                                                ENV['BASIC_AUTH_PASSWORD'])
+    @headers = { 'Authorization' => basic_auth }
+  end
+
+  describe 'GET #show' do
+    it 'showアクションにリクエストすると正常にレスポンスが返ってくる' do
+      get look_for_paper_path(@user), headers: @headers
+      expect(response.status).to eq 200
+    end
+
+    it 'showアクションにリクエストするとレスポンスに投稿済みのlook_for_paperが存在する' do
+      get look_for_paper_path(@user), headers: @headers
+      expect(response.body).to include(@look_for_paper.look_for_paper)
+      expect(response.body).to include(@look_for_paper.journal)
+      expect(response.body).to include(@look_for_paper.search_word)
+    end
+
+    it 'showアクションにリクエストするとレスポンスにlook_for_paperのフォームが存在する' do
+      get look_for_paper_path(@user), headers: @headers
+      expect(response.body).to include('探したい論文')
+      expect(response.body).to include('ジャーナル')
+      expect(response.body).to include('検索ワード')
+    end
+  end
+
+  describe 'POST #create' do
+    context '有効なパラメータの場合' do
+      it 'look_for_paperが作成される' do
+        expect do
+          post look_for_papers_path(@user), params: { look_for_paper: { look_for_paper: 'test', user_id: @user.id } },
+                                            headers: @headers
+        end.to change(LookForPaper, :count).by(1)
+      end
+
+      it 'look_for_paperにリダイレクトされる' do
+        post look_for_papers_path(@user), params: { look_for_paper: { look_for_paper: 'test', user_id: @user.id } },
+                                          headers: @headers
+        expect(response).to redirect_to(look_for_paper_path(@user))
+      end
+    end
+
+    context '無効なパラメータの場合' do
+      it 'look_for_paperは作成されない' do
+        expect do
+          post look_for_papers_path(@user), params: { look_for_paper: { look_for_paper: '', user_id: @user.id } },
+                                            headers: @headers
+        end.not_to change(LookForPaper, :count)
+      end
+    end
+  end
+end
