0a1d16c85d9693ae1e3845a8c35ea04a9859d9b1
Pokesyoum <syoumpm@gmail.com>
2024-12-10 20:46:19 +0900

    wheneverの修正

--- config/schedule.rb
+++ config/schedule.rb
@@ -19,13 +19,15 @@
 
 # Learn more: http://github.com/javan/whenever
 
-set :environment, 'development'
 set :output, 'log/cron.log'
 
+env :RAILS_ENV, ENV['RAILS_ENV'] || 'development'
 env :PATH, ENV['PATH']
-env :GEM_HOME, ENV['GEM_HOME']
-env :GEM_PATH, ENV['GEM_PATH']
 
-every 1.day, at: '00:00 am' do
-  runner "User.assign_missions"
+job_type :rbenv_runner, %q!eval "$(rbenv init -)"; cd :path && RAILS_ENV=:environment bundle exec bin/rails runner -e :environment ':task' :output!
+
+every 1.day at: '0:00 am' do
+  rbenv_runner "User.assign_missions"
 end
+
+# 開発環境ではcrontab -eでproductionをdevelopmentにするか、データベースをmySQLにすることで動作を確認できた
